fb6ed93da0fb4c6be1dc54228ae449d4dc039bb1
Vashishth Patel <vashishthchaudhary48@gmail.com>
2024-06-29 08:23:06 +0530

    initial configuration

---
+++ engine/config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+// const config = require('config');
+// const db = process.env.mongoURI;
+require("dotenv").config();
+const db = process.env['mongoURI']
+
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(
+      db,
+      {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+      }
+    );
+
+    console.log('MongoDB is Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ engine/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const connectDB = require("./config/db");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+
+var whitelist = ["https://localhost:3000/", "http://localhost:3000" , "http://localhost:3000/"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    console.log("Request comed");
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+const app = express();
+//body-parse
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+// cors
+app.use(cors(corsOptions));
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(express.json());
+
+// link the router
+// app.use(require("./controller/login"));
+
+
+// Middleware
+const middleware = (req, res, next) => {
+  console.log("Hello my middleware");
+  next();
+};
+// middleware();
+
+app.get("/", (req, res) => {
+  console.log("Hello odoo!!");
+  res.send("Hello odoo!!");
+});
+
+app.get("/contact", middleware, (req, res) => {
+  console.log("Hello my contact");
+  res.send("Hello Contact");
+});
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
